b08a530b98bc05d1a14800cfa30ae1c35fa89078
statusdying <63418224+statusdying@users.noreply.github.com>
2024-12-08 17:43:08 +0100

    Added a Login Text

---
+++ src/main/java/com/example/passwordmanager/MainActivity.java
@@ -0,0 +1,228 @@
+package com.example.passwordmanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.concurrent.Executor;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton add_button, authenticate_button;
+
+    TextView login_text;
+
+    Activity activity;
+    MyDatabaseHelper myDB;
+    ArrayList<String> _id, title, username, password, url;
+    CustomAdapter customAdapter;
+
+    private Executor executor;
+
+    private androidx.biometric.BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
+
+    Boolean firstLogIn = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+        authenticate_button = findViewById(R.id.authenticate_button);
+        recyclerView = findViewById(R.id.recyclerView);
+        add_button = findViewById(R.id.add_button);
+        login_text = findViewById(R.id.login_text);
+        executor = ContextCompat.getMainExecutor(this);
+        String mySecondVariable = "loggedOut";
+
+        SharedViewModel viewModel = new ViewModelProvider(this).get(SharedViewModel.class);
+        String myVariable = viewModel.getMyVariable();
+
+        if(getIntent().getStringExtra("loggedIn") != null)
+        {
+            mySecondVariable = getIntent().getStringExtra("loggedIn");
+            //Toast.makeText(this, mySecondVariable, Toast.LENGTH_SHORT).show();
+        }
+        //String mySecondVariable = getIntent().getStringExtra("loggedIn");
+
+
+        //Toast.makeText(this, myVariable, Toast.LENGTH_SHORT).show();
+            if(myVariable == "first_state" && mySecondVariable == "loggedOut"){
+                loggedOut();
+                //Toast.makeText(this, firstLogIn.toString(), Toast.LENGTH_SHORT).show();
+
+            }else{loggedIn();}
+
+
+
+
+
+        biometricPrompt = new androidx.biometric.BiometricPrompt(MainActivity.this, executor, new androidx.biometric.BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(MainActivity.this, "Authentication error: " + errString, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull androidx.biometric.BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(MainActivity.this, "Authentication successful", Toast.LENGTH_SHORT).show();
+                firstLogIn = true;
+                viewModel.setMyVariable("second");
+                loggedIn();
+
+
+
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(MainActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        promptInfo = new androidx.biometric.BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric Authentication")
+                .setSubtitle("Login using fingerprint or face")
+                .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG | BiometricManager.Authenticators.DEVICE_CREDENTIAL)
+                                .build();
+
+
+
+        authenticate_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                biometricPrompt.authenticate(promptInfo);
+                //Intent intent = new Intent(MainActivity.this, FingerprintActivity.class);
+                //startActivity(intent);
+            }
+        });
+
+
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                //activity.startActivityForResult(intent,1);
+                startActivity(intent);
+
+            }
+        });
+
+        myDB = new MyDatabaseHelper(MainActivity.this);
+        _id = new ArrayList<>();
+        title = new ArrayList<>();
+        username = new ArrayList<>();
+        password = new ArrayList<>();
+        url = new ArrayList<>();
+
+        storeDataInArrays();
+
+        customAdapter = new CustomAdapter(MainActivity.this,this,_id, title, username, password, url);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Toast.makeText(this, "OnActivityResult", Toast.LENGTH_SHORT).show();
+        if(requestCode == 1){
+            recreate();
+        }
+
+    }
+
+    void storeDataInArrays(){
+        Cursor cursor = myDB.readAllData();
+        if(cursor.getCount() == 0){
+            Toast.makeText(this, "No data.", Toast.LENGTH_SHORT).show();
+        }else{
+            while (cursor.moveToNext()){
+                _id.add(cursor.getString(0));
+                title.add(cursor.getString(1));
+                username.add(cursor.getString(2));
+                password.add(cursor.getString(3));
+                url.add(cursor.getString(4));
+
+            }
+
+        }
+    }
+
+    void loggedIn(){
+        add_button.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.VISIBLE);
+        authenticate_button.setVisibility(View.INVISIBLE);
+        login_text.setVisibility(View.INVISIBLE);
+
+    }
+
+    void loggedOut(){
+        recyclerView.setVisibility(View.INVISIBLE);
+        add_button.setVisibility(View.INVISIBLE);
+        authenticate_button.setVisibility(View.VISIBLE);
+        login_text.setVisibility(View.VISIBLE);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(15 * 60 * 1000); // 10 minutes in milliseconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                // Use a Handler to post the action to the main thread
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Perform your action here on the main thread
+                        // e.g., update UI, show a notification
+                        loggedOut();
+                        //Toast.makeText(MainActivity.this, "Logged out", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }).start();
+    }
+}
---
+++ src/main/java/com/example/passwordmanager/MyDatabaseHelper.java
@@ -0,0 +1,105 @@
+package com.example.passwordmanager;
+
+import static androidx.core.app.ActivityCompat.recreate;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private Context context;
+    private static final String DATABASE_NAME = "CredentialsLibrary.db";
+
+    private static final int DATABASE_VERSION = 2;
+    private static final String TABLE_NAME = "credentials";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_USERNAME = "username";
+    private static final String COLUMN_PASSWORD = "password";
+    private static final String COLUMN_URL = "url";
+
+
+    MyDatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_NAME +
+                " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_TITLE + " TEXT, " +
+                COLUMN_USERNAME + " TEXT, " +
+                COLUMN_PASSWORD + " TEXT, " +
+                COLUMN_URL + " TEXT);";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    void addCredentials(String title, String username, String password, String url) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TITLE, title);
+        cv.put(COLUMN_USERNAME, username);
+        cv.put(COLUMN_PASSWORD, password);
+        cv.put(COLUMN_URL, url);
+        long result = db.insert(TABLE_NAME, null, cv);
+        if(result == -1){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(context, "Added successfully", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    Cursor readAllData(){
+        String query = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db != null){
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor;
+    }
+
+    void updateData(String row_id, String title, String username, String password, String url){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_TITLE, title);
+        cv.put(COLUMN_USERNAME, username);
+        cv.put(COLUMN_PASSWORD, password);
+        cv.put(COLUMN_URL, url);
+
+        long result = db.update(TABLE_NAME, cv, "_id=?", new String[]{row_id});
+        if (result == -1){
+            Toast.makeText(context, "Failed to update", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "Successfully updated",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    void deleteOneRow(String row_id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        long result = db.delete(TABLE_NAME, "_id=?", new String[]{row_id});
+        if(result == -1){
+            Toast.makeText(context, "Failed to delete", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "Successfully deleted", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+}
